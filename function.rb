7a99957ecdfea2db9ede5e98c3a688e466e80a46
Zach Miller <zmiller@ucsb.edu>
2024-10-15 18:02:59 -0700

    First commit

--- function.rb
+++ function.rb
@@ -7,7 +7,65 @@ require 'pp'
 def main(event:, context:)
   # You shouldn't need to use context, but its fields are explained here:
   # https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-  response(body: event, status: 200)
+
+  # handle errors
+  routes = { '/' => 'GET', '/auth/token' => 'POST' }
+
+  if !event.has_key?('path') || !routes.has_key?(event['path'])
+    return response(status: 404)
+  end
+  
+  if !event.has_key?('httpMethod') || event['httpMethod'] != routes[event['path']]
+    return response(status: 405)
+  end
+
+  # '/'
+  if event['path'] == '/'
+    payload = nil
+    if !event.has_key?('headers') || !event['headers'].has_key?('Authorization')
+      return response(status: 403)
+    elsif event['headers']['Authorization'] =~ /^Bearer (\S+)$/
+      begin
+        decoded_jwt = JWT.decode($1, ENV['JWT_SECRET'], true, { algorithm: 'HS256' })
+        payload = decoded_jwt[0]
+      rescue JWT::DecodeError
+        return response(status: 403)
+      end
+    end
+
+    # check that decoded_jwt has all its parts.
+    # The first entry is the encoded payload, 
+    # and the second is the algorithm used to decode
+    required_fields = [ 'data', 'exp', 'nbf' ]
+    if (payload.nil? ||
+        !required_fields.all? { |field| payload.has_key?(field) })
+      return response(status: 403)
+    else
+      return response(body: payload['data'])
+    end
+  end
+
+  # '/auth/token'
+  if event['path'] == '/auth/token'
+    if (!event.has_key?('headers') || 
+        !event['headers'].has_key?('Content-Type') ||
+        event['headers']['Content-Type'] != 'application/json')
+      return response(status: 415)
+    elsif (!event.has_key?('body') ||
+           !valid_json?(event['body']))
+      return response(status: 422)
+    else
+      payload = {
+        data: event['body'],
+        exp: Time.now.to_i + 5,
+        nbf: Time.now.to_i + 2
+      }
+
+      token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
+
+      return response(body: { 'token' => token }, status: 201)
+    end
+  end
 end
 
 def response(body: nil, status: 200)
@@ -15,6 +73,15 @@ def response(body: nil, status: 200)
     body: body ? body.to_json + "\n" : '',
     statusCode: status
   }
+end
+
+def valid_json?(string)
+  begin
+    JSON.parse string
+    true
+  rescue JSON::ParserError
+    false
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
@@ -28,7 +95,7 @@ if $PROGRAM_NAME == __FILE__
                'body' => '{"name": "bboe"}',
                'headers' => { 'Content-Type' => 'application/json' },
                'httpMethod' => 'POST',
-               'path' => '/token'
+               'path' => '/auth/token'
              })
 
   # Generate a token
